6330e9d7e38b08ccffe8d0bce558bf6971668469
Bence Rozsa <robence@gmail.com>
2022-01-25 09:05:35 +0100

    show spinner when loading

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_flash_chat/constants.dart';
 import 'package:flutter_flash_chat/screens/chat_screen.dart';
 import 'package:flutter_flash_chat/widgets/screen_buttons.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const name = '/login';
@@ -17,70 +18,81 @@ class _LoginScreenState extends State<LoginScreen> {
   final auth = FirebaseAuth.instance;
   var email = '';
   var password = '';
+  var isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: kHeroLogo,
-              transitionOnUserGestures: true,
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: kHeroLogo,
+                transitionOnUserGestures: true,
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
+              const SizedBox(
+                height: 48.0,
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+                style: const TextStyle(color: Colors.black),
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 24.0,
-            ),
-            LoginButton(
-              onPressed: () async {
-                try {
-                  final UserCredential? user =
-                      await auth.signInWithEmailAndPassword(
-                          email: email, password: password);
+              const SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              LoginButton(
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  try {
+                    final UserCredential? user =
+                        await auth.signInWithEmailAndPassword(
+                            email: email, password: password);
 
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.name);
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.name);
+                    }
+                  } catch (e) {
+                    print(e);
+                  } finally {
+                    setState(() {
+                      isLoading = false;
+                    });
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            )
-          ],
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
